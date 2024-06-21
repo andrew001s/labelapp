ddc1ea18cdab158844a23e959df17629abe79d86
Shandrew <andresroman45678@gmail.com>
2024-06-21 10:57:51 -0500

    Refactor ImageServiceImpl to remove unused imports

--- src/main/java/com/golden/labelapp/labelapp/dao/ImageServiceImpl.java
+++ src/main/java/com/golden/labelapp/labelapp/dao/ImageServiceImpl.java
@@ -13,7 +13,6 @@ import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
